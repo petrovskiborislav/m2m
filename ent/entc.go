0872b8cda18231e4326aa9eacd0ac99f2da3fc90
Borislav Petrovski <borislav@thebaasco.ca>
2022-07-14 17:11:14 +0300

    commit

---
+++ ent/entc.go
@@ -0,0 +1,25 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"errors"
+	"log"
+
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+)
+
+func main() {
+	ex, err := entgql.NewExtension(
+		entgql.WithConfigPath("./gqlgen.yml"),
+	)
+	if !errors.Is(err, nil) {
+		log.Fatalf("Error: failed creating entgql extension: %v", err)
+	}
+	if err := entc.Generate("./ent/schema", &gen.Config{}, entc.Extensions(ex)); !errors.Is(err, nil) {
+		log.Fatalf("Error: failed running ent codegen: %v", err)
+	}
+}
